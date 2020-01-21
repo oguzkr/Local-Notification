7678d501e10398112ed76f089921c413b0da3c8f
oguzkr <oguzkaratoruk@yandex.com>
2020-01-21 10:46:32 +0300

    Initial Commit

---
+++ Local Notification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Local Notification
+//
+//  Created by Oğuz Karatoruk on 21.01.2020.
+//  Copyright © 2020 Oğuz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
