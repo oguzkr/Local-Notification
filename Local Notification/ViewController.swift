cf4168d70b890d30f71fa00e16b27214f08472cb
oguzkr <oguzkaratoruk@yandex.com>
2020-01-21 10:51:37 +0300

    Update ViewController.swift

--- Local Notification/ViewController.swift
+++ Local Notification/ViewController.swift
@@ -12,7 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scheduleLocalNotification(hour: 09, minute: 05, title: "LOCAL NOTIFICATION", body: "github.com/oguzkr")
+    }
+    
+    func registerLocalNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Success")
+            } else {
+                print("Error")
+            }
+        }
+    }
+
+    func scheduleLocalNotification(hour:Int, minute:Int, title:String, body:String) {
+        registerLocalNotification()
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.categoryIdentifier = "notification"
+        content.userInfo = ["key": "data"]
+        content.sound = UNNotificationSound.default
+        var dateComponents = DateComponents()
+        dateComponents.hour = 14
+        dateComponents.minute = 52
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 
 
